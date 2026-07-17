8a82fda0dd7f2854e51979474596ed476b3620fe
ajinkyad015 <ajinkyadd.2023@gmail.com>
2026-07-17 22:23:55 +0530

    Added Lifespan

--- app/main.py
+++ app/main.py
@@ -1,26 +1,45 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.v1.router import api_router
-from app.exception_handlers import register_exception_handlers
-
 from app.core.config import settings
-
 from app.core.logging import setup_logging
+from app.exception_handlers import register_exception_handlers
 from app.middleware.logging import RequestLoggingMiddleware
 
-setup_logging()
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    setup_logging()
+
+    logger.info("Starting IssueForge API...")
+
+    # Example application resource
+    app.state.started_at = "Application Started"
+
+    logger.info("Application resources initialized.")
+
+    yield
+
+    logger.info("Cleaning up application resources...")
+
+    del app.state.started_at
+
+    logger.info("Application shutdown complete.")
 
 
 app = FastAPI(
     title=settings.app_name,
     version=settings.app_version,
+    lifespan=lifespan,
 )
+
 app.add_middleware(RequestLoggingMiddleware)
 
 register_exception_handlers(app)
 
-
-app.include_router(
-    api_router,
-    prefix="/api/v1",
-)
+app.include_router(api_router)
